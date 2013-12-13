ba886c303eee49fbac2f8b3d8af1e87e8e3fe531
shariffh <shahuma2010@gmail.com>
2013-12-13 07:26:14 +0300

    More exercises about ordinal and cardinal numbers

--- ex34.rb
+++ ex34.rb
@@ -1,2 +1,10 @@
-animals = ['bear', 'tiger', 'penguin', 'zebra']
-bear = animals[0] #accessing the first animal
+#animals = ['bear', 'tiger', 'penguin', 'zebra']
+#bear = animals[0] #accessing the first animal
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+python = animals[1]    #The animal at 1 is a python
+peacock = animals[2]   #The 3rd animal is at 2 and is a peacock
+bear = animals[0]      #The 1st animal is at 0 and is a bear
+kangaroo = animals[3]  #The animal at 3 is the 4th animal and is a kangaroo
+whale = animals[4]     #The 5th animal is at 4 and is a whale
+platypus = animals[5]  #The 6th animal is at 5 and is a platypus
