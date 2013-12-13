00c58d7a3f743065d7650de102753d7e7cc2cf52
shariffh <shahuma2010@gmail.com>
2013-12-13 12:33:55 +0300

    commod words used

---
+++ ex22.rb
@@ -0,0 +1,13 @@
+puts " " # print output in a new line
+print " " # print output without a new line
+def # create/define function
+end # end function
+STDIN.gets.chomp() # get user input in arguments
+STDIN.gets #
+prompt '' # ask user to enter something (ask user input)
+gets.chomp() # get user input
+puts "#{}" # '#{}' - shows variable in output
+\n # next line
+File.open() # open File
+ARGV # argument variable - holds the arguments passed in scripts when they are running
+*args # arguments (any) - same as ARGV
