aff0517a9d1731885e115c33709db2526c775fdd
shariffh <shahuma2010@gmail.com>
2013-12-13 12:15:21 +0300

    Inheritance and Composition

--- ex44.rb
+++ ex44.rb
@@ -25,15 +25,61 @@ class Child < Parent
         puts "CHILD, AFTER PARENT altered()"
     end
 end
+puts "\n INHEHERITANCE"
+puts "**************"
+dad = Parent.new() # PARENT implicit()
+son = Child.new()  # PARENT implicit()
 
-dad = Parent.new()
+dad.implicit()     # PARENT override()
+son.implicit()     # CHILD implicit()
+
+dad.override()     # PARENT altered()
+son.override()     # CHILD, BEFORE PARENT altered()
+
+dad.altered()      # PARENT altered()
+son.altered()      # CHILD, AFTER PARENT altered()
+
+
+puts "\n COMPOSITION"
+puts "**************"
+class Other
+
+    def override()
+        puts "OTHER override()"
+    end
+
+    def implicit()
+        puts "OTHER implicit()"
+    end
+
+    def altered()
+        puts "OTHER altered()"
+    end
+end
+
+class Child
+
+    def initialize()
+        @other = Other.new()
+    end
+
+    def implicit()
+        @other.implicit()
+    end
+    
+    def override()
+        puts "CHILD override()"
+    end
+
+    def altered()
+        puts "CHILD, BEFORE OTHER altered()"
+        @other.altered()
+        puts "CHILD, AFTER OTHER altered()"
+    end
+end
+
 son = Child.new()
 
-dad.implicit()
 son.implicit()
-
-dad.override()
 son.override()
-
-dad.altered()
 son.altered()
