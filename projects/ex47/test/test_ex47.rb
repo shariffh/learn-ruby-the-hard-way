80e9e0d2eb539f99f9fa1c07477536ec62c55a59
shariffh <shahuma2010@gmail.com>
2013-12-18 06:16:50 +0300

    add code to test

--- projects/ex47/test/test_ex47.rb
+++ projects/ex47/test/test_ex47.rb
@@ -4,9 +4,7 @@ require_relative '../lib/ex47'
 class MyUnitTests < Test::Unit::TestCase
 
   def test_room()
-    gold = Room.new("GoldRoom",
-                    """This room has gold in it you can grab. There's a
-door to the north.""")
+    gold = Room.new("GoldRoom","""This room has gold in it you can grab. There's a door to the north.""")
     assert_equal(gold.name, "GoldRoom")
     assert_equal(gold.paths, {})
   end
