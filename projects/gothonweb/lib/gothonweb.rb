573ef9e77d0996a164f37ee2c5f8c817690b8d89
shariffh <shahuma2010@gmail.com>
2013-12-29 18:41:09 +0300

    make simple hello world project

--- projects/gothonweb/lib/gothonweb.rb
+++ projects/gothonweb/lib/gothonweb.rb
@@ -1,35 +1,9 @@
 require_relative "gothonweb/version"
-require_relative "map"
 require "sinatra"
-require "erb"
 
 module Gothonweb
-
-  use Rack::Session::Pool
-
   get '/' do
-    # this is used to "setup" the session with starting values
-    p START
-    session[:room] = START
-    redirect("/game")
+    greeting = "Hello, World!"
+    return greeting
   end
-
-  get '/game' do
-    if session[:room]
-      erb :show_room, :locals => {:room => session[:room]}
-    else
-      # why is this here? do you need it?
-      erb :you_died
-    end
-  end
-
-  post '/game' do
-    action = "#{params[:action] || nil}"
-    # there is a bug here, can you fix it?
-    if session[:room]
-      session[:room] = session[:room].go(params[:action])
-    end
-    redirect("/game")
-  end
-
 end
